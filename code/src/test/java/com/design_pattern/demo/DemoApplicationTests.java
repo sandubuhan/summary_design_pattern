5db44a1be6d6e930b7391c689c8a053447e4b824
sandubuhan <54792286+sandubuhan@users.noreply.github.com>
2022-03-20 10:27:45 +0800

    新增code目录，为以后存放代码地址

---
+++ code/src/test/java/com/design_pattern/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design_pattern.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
